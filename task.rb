010166b716f2ff2881ab48bcc06a6f2d1d9a5468
shutheonetokyo <ss1030ss@icloud.com>
2021-03-11 16:36:23 +0900

    Q1~Q4修正版

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋",]
 
   # 以下に回答を記載
-  name.push("斎藤")
+  names << "斎藤"
   p names
 end
 
@@ -28,7 +28,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -38,21 +38,22 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p [].empty?
+  p [1, 2, 3].empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+p numbers1 * 10
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
