a3cc6c12517af6a56facf49aa8561a1db12a4fad
shutheonetokyo <ss1030ss@icloud.com>
2021-03-12 23:11:20 +0900

    Q6,Q8 修正

--- task.rb
+++ task.rb
@@ -46,7 +46,8 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-p numbers1 * 10
+  numbers2 = numbers1.map { |x| x * 10 }
+  p numbers2
 end
 
 def q7
@@ -62,7 +63,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  
+  programming_languages.map(&:capitalize!)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -79,7 +81,6 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
 end
 
 def q11
