ecca94c8007baab87aa5b61940f22bcee6c867da
shutheonetokyo <ss1030ss@icloud.com>
2021-03-14 21:20:11 +0900

    Q11修正版

--- task.rb
+++ task.rb
@@ -96,7 +96,9 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  sports.flatten.uniq.each.with_index(1) do |sport, i|
+    puts "No.#{i} #{sport}"
+  end
 end
 
 def q12
@@ -111,14 +113,14 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_date.replace
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  p data.keys
 end
 
 def q15
@@ -138,7 +140,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do|user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
