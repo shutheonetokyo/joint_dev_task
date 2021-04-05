8e895e85145d9ba01204a09d02f6ac4acff71e98
shutheonetokyo <ss1030ss@icloud.com>
2021-04-05 22:26:48 +0900

    Q20修正

--- task.rb
+++ task.rb
@@ -230,18 +230,18 @@ class Zoo
   end
 
   def info_entry_fee(user)
-    case user.age
-      when 0..5
-        puts "#{user.name}さんの入場料金は#{@entry_fee[:infant]}円です。"
-      when 6..12
-        puts "#{user.name}さんの入場料金は#{@entry_fee[:children]}円です。"
-      when 13..64
-        puts "#{user.name}さんの入場料金は#{@entry_fee[:adult]}円です。"
-      when 65..120
-        puts "#{user.name}さんの入場料金は#{@entry_fee[:senior]}円です。"
+    user_entry_fee = case user.age
+    when 0..5
+      @entry_fee[:infant]
+    when 6..12
+      @entry_fee[:children]
+    when 13..64
+      @entry_fee[:adult]
+    when 65..120
+      @entry_fee[:senior]
     end
+    puts "#{user.name}さんの入場料金は#{user_entry_fee}円です。"
   end
-
 end
 
 def q20
